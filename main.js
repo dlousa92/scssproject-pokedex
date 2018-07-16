24f2d55431926b4f3c25469470ba5e14361ed8da
Daniel Lousa <dlousa92@gmail.com>
2018-07-16 12:40:52 -0400

    write out pokemon card selectors

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const url = 'http://pokeapi.co/api/v2/'
 
 // These variables will work with the search form to pull the value of what was searched
 const search = document.querySelector('.pokedexsearch')
-const name = document.querySelector('.searchvalue')
+const nameQuery = document.querySelector('.searchvalue')
+const pokemonName = document.querySelector('.pokemonname')
+const pokemonSprite = document.querySelector('.pokemonsprite')
+const pokemonInfo = document.querySelector('.pokemoninfo')
 
 // This is the function that runs when the Search button is clicked, users will search for Pokemon by name
 function searchPokedex (e) {
   e.preventDefault()
   console.log('Search was clicked')
-  console.log(name.value)
+  console.log(nameQuery.value)
 
-  fetch(`${url}pokemon/${name.value}`)
+  fetch(`${url}pokemon/${nameQuery.value}`)
     .then(function (res) {
       console.log(res)
       return res.json()
