d4b162761bcb086eb7b082870fc1098c10ab1715
Daniel Lousa <dlousa92@gmail.com>
2018-07-16 12:35:18 -0400

    add comments

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 const url = 'http://pokeapi.co/api/v2/'
 
+// These variables will work with the search form to pull the value of what was searched
 const search = document.querySelector('.pokedexsearch')
 const name = document.querySelector('.searchvalue')
 
-function updateField (e) {
-  e.preventDefault()
-}
-
+// This is the function that runs when the Search button is clicked, users will search for Pokemon by name
 function searchPokedex (e) {
   e.preventDefault()
   console.log('Search was clicked')
+  console.log(name.value)
 
-  fetch(`${url}pokemon/${name}`)
+  fetch(`${url}pokemon/${name.value}`)
     .then(function (res) {
       console.log(res)
       return res.json()
@@ -21,5 +20,4 @@ function searchPokedex (e) {
     })
 }
 
-name.addEventListener('onChange', updateField)
 search.addEventListener('submit', searchPokedex)
