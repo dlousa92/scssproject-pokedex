d1a874dc608e143f9453fd26803f726085491429
Daniel Lousa <dlousa92@gmail.com>
2018-07-16 12:15:31 -0400

    create initial functions

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 const url = 'http://pokeapi.co/api/v2/'
+
+const search = document.querySelector('.pokedexsearch')
+const name = document.querySelector('.searchvalue')
+
+function updateField (e) {
+  e.preventDefault()
+}
+
+function searchPokedex (e) {
+  e.preventDefault()
+  console.log('Search was clicked')
+
+  fetch(`${url}pokemon/${name}`)
+    .then(function (res) {
+      console.log(res)
+      return res.json()
+    })
+    .then(function (pokemon) {
+      console.log(pokemon)
+    })
+}
+
+name.addEventListener('onChange', updateField)
+search.addEventListener('submit', searchPokedex)
